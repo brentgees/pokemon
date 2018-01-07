e2198f270534d4b07a66278430026fabe119a799
Brent Gees <brent@dropsolid.com>
2018-01-07 20:09:09 +0100

    migrate

--- docroot/modules/custom/pokemon_migrate/src/Controller/PokemonMigrateBatchController.php
+++ docroot/modules/custom/pokemon_migrate/src/Controller/PokemonMigrateBatchController.php
@@ -7,24 +7,32 @@
 
 namespace Drupal\pokemon_migrate\Controller;
 
+use Pokemon\Pokemon;
+
 /**
  * Class PokemonMigrateBatchController.
  *
  * @package Drupal\pokemon_migrate\Controller
  */
 class PokemonMigrateBatchController {
+
+  /**
+   * Perform batch.
+   */
   public function content() {
+    $sets = Pokemon::Set()->all();
 
     $batch = array(
       'title' => t('Exporting'),
       'operations' => array(
-        array('migrate_sets'),
+        array('pokemon_migrate_sets', array($sets)),
       ),
       'finished' => 'pokemon_migrate_finished_callback',
       'file' => drupal_get_path('module', 'pokemon_migrate') . '/pokemon_migrate.migrate.inc',
     );
 
     batch_set($batch);
-    return batch_process('user');
+    return batch_process('<front>');
+    //    return batch_process('user');
   }
 }
