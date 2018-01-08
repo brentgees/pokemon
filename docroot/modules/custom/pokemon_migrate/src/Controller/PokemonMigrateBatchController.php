9d38d89556a9434acd8b4b918009974774ef7447
Brent Gees <brent@dropsolid.com>
2018-01-08 23:08:55 +0100

    update migrate

--- docroot/modules/custom/pokemon_migrate/src/Controller/PokemonMigrateBatchController.php
+++ docroot/modules/custom/pokemon_migrate/src/Controller/PokemonMigrateBatchController.php
@@ -20,12 +20,17 @@ class PokemonMigrateBatchController {
    * Perform batch.
    */
   public function content() {
+    // Get previously imported nodes.
+    $query = \Drupal::database()->select('pokemon_migrate', 'pm');
+    $query->fields('pm');
+    $result = $query->execute()->fetchAllAssoc('ptcgapi_id');
+
     $sets = Pokemon::Set()->all();
 
     $batch = array(
       'title' => t('Exporting'),
       'operations' => array(
-        array('pokemon_migrate_sets', array($sets)),
+        array('pokemon_migrate_sets', array($sets, $result)),
       ),
       'finished' => 'pokemon_migrate_finished_callback',
       'file' => drupal_get_path('module', 'pokemon_migrate') . '/pokemon_migrate.migrate.inc',
@@ -33,6 +38,5 @@ class PokemonMigrateBatchController {
 
     batch_set($batch);
     return batch_process('<front>');
-    //    return batch_process('user');
   }
 }
