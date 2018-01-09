8a4e297108d5ab201d17a4b8a98a309d71a8603e
Brent Gees <brent@dropsolid.com>
2018-01-09 18:58:04 +0100

    start migrate cards

--- docroot/modules/custom/pokemon_migrate/src/Controller/PokemonMigrateBatchController.php
+++ docroot/modules/custom/pokemon_migrate/src/Controller/PokemonMigrateBatchController.php
@@ -29,6 +29,8 @@ class PokemonMigrateBatchController {
     // Get previously imported nodes.
     $query = \Drupal::database()->select('pokemon_migrate', 'pm');
     $query->fields('pm');
+    $query->condition('entity_type', 'taxonomy');
+    $query->condition('bundle', 'set');
     $result = $query->execute()->fetchAllAssoc('ptcgapi_id');
 
     $sets = Pokemon::Set()->all();
@@ -37,6 +39,38 @@ class PokemonMigrateBatchController {
       'title' => t('Exporting'),
       'operations' => array(
         array('pokemon_migrate_sets', array($sets, $result)),
+      ),
+      'finished' => 'pokemon_migrate_finished_callback',
+      'file' => drupal_get_path('module', 'pokemon_migrate') . '/pokemon_migrate.migrate.inc',
+    );
+
+    batch_set($batch);
+    return batch_process('<front>');
+  }
+
+  /**
+   * Migrates the cards
+   */
+  public function migrateCards() {
+    // Delete nodes
+    $result = \Drupal::entityQuery('node')
+      ->condition('type', 'card')
+      ->execute();
+    entity_delete_multiple('node', $result);
+
+    // Get previously imported nodes.
+    $query = \Drupal::database()->select('pokemon_migrate', 'pm');
+    $query->fields('pm');
+    $query->condition('entity_type', 'node');
+    $query->condition('bundle', 'card');
+    $result = $query->execute()->fetchAllAssoc('ptcgapi_id');
+
+    $cards = Pokemon::Card()->where(['setCode' => 'xy7'])->all();
+
+    $batch = array(
+      'title' => t('Exporting'),
+      'operations' => array(
+        array('pokemon_migrate_cards', array($cards, $result)),
       ),
       'finished' => 'pokemon_migrate_finished_callback',
       'file' => drupal_get_path('module', 'pokemon_migrate') . '/pokemon_migrate.migrate.inc',
